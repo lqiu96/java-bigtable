981f73fd806e62b55a3100b183cc5a811cf10939
Lawrence Qiu <lawrenceqiu@google.com>
2024-06-25 11:33:42 -0400

    chore: Update the JAVA_HOME value

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -30,7 +30,7 @@ echo ${JOB_TYPE}
 current_java_home=$JAVA_HOME
 pushd /tmp
 wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz
-tar zxvf jdk-17_linux-x64_bin.tar.gz -C java-17
+tar -zxvf jdk-17_linux-x64_bin.tar.gz java-17
 export JAVA_HOME=/tmp/java-17
 export PATH=$PATH:$JAVA_HOME/bin
 popd
